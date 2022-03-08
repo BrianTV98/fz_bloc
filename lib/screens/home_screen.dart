b7325ee987274623d6caabcc148fdbfd3a391dbb
Van Hieu Tran <tvhieu>
2022-03-08 23:42:54 +0700

    'fix'

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -7,31 +7,16 @@ import '../blocs/app_state.dart';
 import '../widgets/widgets.dart';
 
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({required this.title,Key? key}) : super(key: key);
 
   final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text(title),
       ),
       body: BlocProvider(
         create: (_)=> AppCubit(),
@@ -45,3 +30,4 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+
